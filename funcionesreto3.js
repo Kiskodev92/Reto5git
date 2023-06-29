8dae09a4f236e2dc54a61be9d7203809bc9b6ee8
Kiskotkd <fjgp92@hotmail.com>
2023-06-29 09:51:44 +0200

    rama2

--- funcionesreto3.js
+++ funcionesreto3.js
@@ -1,3 +1,7 @@
+function suma(x, y, z){
+    console.log(x + y + z);
+};
+
 function resta(x, y){
     console.log(x-y)
 };
@@ -13,7 +17,7 @@ function div(x, y){
 function cuadrado(x,y){
     console.log(Math.pow(x, 2), Math.pow(y, 2));
 }
-suma(12, 2);
+suma(12, 2, 66);
 resta(12, 2);
 producto(12, 2);
 div(12, 2);
