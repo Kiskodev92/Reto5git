2074608add1e61361d45d6f4301280b523c792d3
Kiskotkd <fjgp92@hotmail.com>
2023-06-29 09:39:57 +0200

    añadido

---
+++ funcionesreto3.js
@@ -0,0 +1,24 @@
+function suma(x, y){
+    console.log(x+y)
+};
+
+function resta(x, y){
+    console.log(x-y)
+};
+
+function producto(x, y){
+    console.log(x*y)
+};
+
+function div(x, y){
+    console.log(x/y)
+};
+
+function cuadrado(x,y){
+    console.log(Math.pow(x, 2), Math.pow(y, 2));
+}
+suma(12, 2);
+resta(12, 2);
+producto(12, 2);
+div(12, 2);
+cuadrado(2,3);
