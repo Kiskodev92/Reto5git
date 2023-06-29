af7400401225dd469c480a3d1a4891c7d53875c9
Kiskotkd <fjgp92@hotmail.com>
2023-06-29 10:07:30 +0200

    mods

---
+++ README.md
@@ -0,0 +1,4 @@
+Hemos creado las dos ramas con git branch,
+una vez se han creado se han editado y añadido con git add, git commit y git push,
+una vez subido a github procedo a unirlo todo con git merge en la rama principal
+cambiando todo lo que hemos cambiado en las dos ramas anteriores.
--- funcionesreto3.js
+++ funcionesreto3.js
@@ -1,19 +1,23 @@
+function suma(x, y, z){
+    console.log(x + y + z);
+};
+
 function resta(x, y){
-    console.log(x-y)
+    console.log(x-y);
 };
 
 function producto(x, y){
-    console.log(x*y)
+    console.log(x*y);
 };
 
 function div(x, y){
-    console.log(x/y)
+    console.log(x/y);
 };
 
 function cuadrado(x,y){
     console.log(Math.pow(x, 2), Math.pow(y, 2));
 }
-suma(12, 2);
+suma(12, 2, 66);
 resta(12, 2);
 producto(12, 2);
 div(12, 2);
